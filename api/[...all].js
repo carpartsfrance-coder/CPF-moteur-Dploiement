a9b83f1f50ac2059a3c01caa6a46e1a33cf4083a
killian13220 <88785688+killian13220@users.noreply.github.com>
2025-11-09 18:00:43 +0100

    Fix: Vercel function en ESM (export default) pour corriger erreur 'module is not defined'

--- api/[...all].js
+++ api/[...all].js
@@ -1,10 +1,14 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   try {
     const mod = await import("../server/index.js");
-    const app = mod.default;
-    return app(req, res);
+    const app = mod.default || mod.app || mod.server;
+    if (typeof app === 'function') {
+      return app(req, res);
+    }
+    res.statusCode = 500;
+    res.end("Server app not exported");
   } catch (err) {
     res.statusCode = 500;
     res.end("Internal Server Error");
   }
-};
+}
